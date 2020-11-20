c737a844806d3ca4355a5467cde082c5e5774a0e
Saif <xpyrophoenix21@gmail.com>
2020-11-20 17:52:54 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+use std::fs::ReadDir;
+use std::{fs, path::Path};
+
+use anyhow::Result;
+use walkdir::WalkDir;
+
+struct FileEntry {
+    name: String,
+    size: usize,
+}
+
+impl Clone for FileEntry {
+    fn clone(&self) -> Self {
+        FileEntry {
+            name: self.name.clone(),
+            size: self.size,
+        }
+    }
+}
+
+impl FileEntry {
+    pub fn new(name: String, size: usize) -> FileEntry {
+        FileEntry {
+            name: name,
+            size: size,
+        }
+    }
+
+    pub fn sort(vec: &mut Vec<FileEntry>) {
+        vec.sort_by(|a, b| b.size.cmp(&a.size));
+    }
+}
+
+fn main() {
+    match start("C:/Program Files") {
+        Ok(_) => println!("started successfully"),
+        Err(err) => println!("error: {}", err),
+    }
+}
+
+fn start(path: &str) -> Result<()> {
+    let mut vec: Vec<FileEntry> = vec![];
+    for folder in get_contents(path) {
+        let folder = folder?;
+        let name = folder.file_name();
+        let name = name.to_str().unwrap();
+
+        let path = folder.path();
+        let size = get_size(&path).unwrap();
+
+        let entry = FileEntry::new(String::from(name), size);
+        vec.push(entry);
+    }
+    FileEntry::sort(&mut vec);
+
+    for entry in vec {
+        let name = entry.name;
+        let size = entry.size;
+        let mb: f32 = entry.size as f32 / (1024.0 * 1024.0);
+        println!("{}: {}, ({} MB)", name, size, mb);
+    }
+
+    Ok(())
+}
+
+fn get_contents(root: &str) -> ReadDir {
+    return fs::read_dir(root).unwrap();
+}
+
+fn walk(root: &str) -> Vec<String> {
+    let mut vec: Vec<String> = vec![];
+    for entry in WalkDir::new(root).into_iter().filter_map(|e| e.ok()) {
+        let path: &str = entry.path().to_str().unwrap();
+        vec.push(String::from(path));
+    }
+    return vec;
+}
+
+fn get_size(path: &Path) -> Result<usize> {
+    let folder = path.to_str().unwrap();
+    let mut size: usize = 0;
+    if path.is_file() {
+        return Ok(get_file_size(path.to_str().unwrap()));
+    }
+    for file in walk(folder) {
+        size += get_file_size(&file);
+    }
+    return Ok(size);
+}
+
+fn get_file_size(path: &str) -> usize {
+    let res = fs::metadata(path);
+    let metadata = match res {
+        Ok(val) => Some(val),
+        Err(_) => None,
+    };
+    if metadata.is_none() {
+        return 0;
+    }
+    let size = metadata.unwrap().len();
+    return size as usize;
+}
